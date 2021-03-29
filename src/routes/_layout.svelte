87c51add0fefa75f06220f3962bed9987c595368
Grant Yap <grantivanyap@yahoo.com>
2021-03-30 01:19:29 +0800

    Remove unused code

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -13,13 +13,7 @@
     position: relative;
     max-width: 100%;
     background-color: white;
-    /* padding: 2em; */
     margin: 0 auto;
     box-sizing: border-box;
-  }
-
-  slot {
-    max-width: 56em;
-    padding: 2em;
   }
 </style>
