7e43e68a1f9a0ad727147a460619c9ec6d98ee85
Grant Yap <grantivanyap@yahoo.com>
2021-04-01 18:56:32 +0800

    Add required middleware to the webserver
    Apparently, the StackOverflow solutions we were following needed
    middleware for keeping session data and reading the body of POST
    requests.
    
    This patch fixes that by including 'express-session' and 'body-parser'.
    
    You'll probably have to run `npm install` after this patch.

--- src/server.js
+++ src/server.js
@@ -2,19 +2,31 @@ import sirv from "sirv";
 import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
+import session from "express-session";
+import { json } from "body-parser";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
   .use(
+    session({
+      secret: "SuperSecretShouldNotBeCommitted",
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 31536000,
+      },
+    })
+  )
+  .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
+    json(),
     sapper.middleware({
       session: (req, res) => ({
-        user_id:
-          req.session && req.session.user_id ? req.session.user_id : null,
-        cards: req.session && req.session.cards ? req.session.cards : null,
+        user_id: req.session.user_id,
+        cards: req.session.cards,
       }),
     })
   )
