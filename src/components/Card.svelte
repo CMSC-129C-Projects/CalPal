aaf3d446aeeb596d20e3aecaf50a8ad935603822
Thomas Garcia <trgarcia1@up.edu.ph>
2021-04-07 23:42:07 +0800

    Implement Card.svelte

---
+++ src/components/Card.svelte
@@ -0,0 +1,61 @@
+<script>
+  import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Modal,
+  } from "sveltestrap/src";
+</script>
+
+<div class="parent">
+  <Button class="cardButton">
+    <Card class="actualCard">
+      <CardBody class="cardBody">
+        <CardTitle class="cardTitle">John Smith's Birthday</CardTitle>
+        <CardSubtitle class="dueDate">January 1, 2022</CardSubtitle>
+      </CardBody>
+    </Card>
+  </Button>
+</div>
+
+<style>
+  .parent :global(.cardButton) {
+    background-color: transparent;
+    padding: 0%;
+    margin-bottom: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
+    border: none;
+    color: black;
+    text-align: left;
+    width: 230px;
+  }
+
+  .parent :global(.actualCard) {
+    /* margin: 10px; */
+    /* border-width: 0.15em; */
+    border-color: #d9d9d9;
+    /* height: 65px; */
+    /* background-color: lightgreen; */
+  }
+
+  .parent :global(.cardBody) {
+    padding-top: 0%;
+    padding-bottom: 0%;
+    padding-left: 5%;
+  }
+
+  .parent :global(.cardTitle) {
+    margin-bottom: 2%;
+    font-size: 1.25em;
+  }
+
+  .parent :global(.dueDate) {
+    padding-top: 0%;
+    font-size: 0.9em;
+  }
+</style>
