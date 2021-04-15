ea0dad2c9dcf962ae0d4a596ec3c4c67d272d1cf
Dain Angelo Parcon <dainangeloparcon@gmail.com>
2021-04-15 18:25:02 +0800

    Handle behavior of empty card due date

--- src/components/Reminder.svelte
+++ src/components/Reminder.svelte
@@ -1,13 +1,20 @@
 <script>
   import { Alert } from "sveltestrap/src";
-
+  import formattedDate from "../routes/_date-format.js";
   export let card;
 
-  $: dueDate = new Date(card.due_date_time);
+  $: dueDate = formattedDate(new Date(card.due_date_time));
+  $: {
+    if (card.due_date_time === "") {
+      card.remind_date_time = "";
+    }
+  }
 </script>
 
 {#if !card.is_archived}
-  {#if Date.now() >= new Date(card.remind_date_time)}
-    <Alert color="warning">This task is due on {dueDate}</Alert>
+  {#if card.remind_date_time != "" && card.due_date_time != ""}
+    {#if Date.now() >= new Date(card.remind_date_time)}
+      <Alert color="warning">This task is due on {dueDate}</Alert>
+    {/if}
   {/if}
 {/if}
