3fbf3dc0732ec6d60ee10c3fc0efee88adc731a0
Grant Yap <grantivanyap@yahoo.com>
2021-04-15 15:18:05 +0800

    Add day of the week to formattedDate()
    This patch also includes the ability to change between using the long or
    short names of the days of the week and the months.

--- src/routes/_date-format.js
+++ src/routes/_date-format.js
@@ -1,19 +1,50 @@
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+const monthNames = {
+  short: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  long: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+};
+const dayNames = {
+  short: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  long: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+};
 
 export default function formattedDate(inputDate) {
   const [month, date, year] = inputDate.toLocaleDateString("en-US").split("/");
-  return `${monthNames[month - 1]} ${date}, ${year}`;
+  const day = inputDate.getDay();
+  return `${dayNames["short"][day]}, ${
+    monthNames["long"][month - 1]
+  } ${date}, ${year}`;
 }
