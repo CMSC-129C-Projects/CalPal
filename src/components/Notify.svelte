a751e95e33d470c569a5e2a01003bef07e6191e0
Thomas Garcia <trgarcia1@up.edu.ph>
2021-04-16 21:36:25 +0800

    Add temporary Notify.svelte
    Notify.svelte is a temporary file for testing the alert-area div

---
+++ src/components/Notify.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Alert } from "sveltestrap/src";
+
+  let visible = true;
+</script>
+
+<Alert color="warning" isOpen={visible} toggle={() => (visible = false)}>
+  I am an alert and I can be dismissed!
+</Alert>
