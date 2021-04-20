05d39d79ca9937f82e8d42b0d991983da3f162d7
Dain Angelo Parcon <dainangeloparcon@gmail.com>
2021-04-20 14:43:22 +0800

    Make archive button unarchive button for archived cards

--- src/components/ArchiveCard.svelte
+++ src/components/ArchiveCard.svelte
@@ -17,26 +17,49 @@
 </script>
 
 <div class="parent">
-  <Button outline color="danger" on:click={toggle}>
-    <Icon name="archive" />
-    Archive Card
-  </Button>
-  <Modal isOpen={open} {toggle}>
-    <ModalHeader {toggle}>Confirm</ModalHeader>
-    <ModalBody>Are you sure you want to archive card?</ModalBody>
-    <ModalFooter>
-      <Button color="secondary" on:click={toggle}>Cancel</Button>
-      <Button
-        color="primary"
-        on:click={() => {
-          is_archived = !is_archived;
-          toggle();
-        }}
-      >
-        Archive
-      </Button>
-    </ModalFooter>
-  </Modal>
+  {#if is_archived}
+    <Button outline color="warning" on:click={toggle}>
+      <Icon name="archive" />
+      Unarchive Card
+    </Button>
+    <Modal isOpen={open} {toggle}>
+      <ModalHeader {toggle}>Confirm</ModalHeader>
+      <ModalBody>Are you sure you want to unarchive card?</ModalBody>
+      <ModalFooter>
+        <Button color="secondary" on:click={toggle}>Cancel</Button>
+        <Button
+          color="primary"
+          on:click={() => {
+            is_archived = !is_archived;
+            toggle();
+          }}
+        >
+          Unarchive
+        </Button>
+      </ModalFooter>
+    </Modal>
+  {:else}
+    <Button outline color="danger" on:click={toggle}>
+      <Icon name="archive" />
+      Archive Card
+    </Button>
+    <Modal isOpen={open} {toggle}>
+      <ModalHeader {toggle}>Confirm</ModalHeader>
+      <ModalBody>Are you sure you want to archive card?</ModalBody>
+      <ModalFooter>
+        <Button color="secondary" on:click={toggle}>Cancel</Button>
+        <Button
+          color="primary"
+          on:click={() => {
+            is_archived = !is_archived;
+            toggle();
+          }}
+        >
+          Archive
+        </Button>
+      </ModalFooter>
+    </Modal>
+  {/if}
 </div>
 
 <style>
