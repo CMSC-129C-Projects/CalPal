20b985c689770691fae1145cb8283bd3751ffd0e
Thomas Garcia <trgarcia1@up.edu.ph>
2021-05-17 14:30:22 +0800

    Add SignIn.svelte
    Create new file named SignIn.svelte. The implementation is currently the skeleton/framework of the log in page.

---
+++ src/components/SignIn.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Col, Container, Row } from "sveltestrap";
+</script>
+
+<div class="sign-in-parent">
+  <Container class="sign-in-container">
+    <Row>
+      <Col sm="12" md={{ size: 6, offset: 3 }}>
+        <Row>
+          <Col class="sign-in-col" sm="12" md={{ size: 6, offset: 3 }}
+            >Log into CalPal
+          </Col>
+        </Row>
+        <Row>
+          <div class="g-signin2" data-onsuccess="onSignIn" />
+        </Row>
+      </Col>
+    </Row>
+  </Container>
+</div>
+
+<style>
+  .sign-in-parent :global(.sign-in-container) {
+    /* background-color: lightcoral; */
+    text-align: center;
+  }
+
+  .sign-in-parent :global(.sign-in-col) {
+    background-color: lightgray;
+    font-size: 1.5em;
+    color: dodgerblue;
+  }
+</style>
