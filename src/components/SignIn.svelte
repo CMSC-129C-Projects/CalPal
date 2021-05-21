ded1e6a62d580deecbae0c56a4e6964f9150eab0
Grant Yap <grantivanyap@yahoo.com>
2021-05-21 15:52:58 +0800

    Try getting user details from the Google Sign In button

--- src/components/SignIn.svelte
+++ src/components/SignIn.svelte
@@ -3,6 +3,21 @@
   import { Card, CardBody } from "sveltestrap/src";
 
   export let isNavBarVisible;
+
+  function onSignIn(googleUser) {
+    console.debug("HERE!");
+
+    const profile = googleUser.getBasicProfile();
+    console.debug(`ID: ${profile.getId()}`);
+    console.debug(`Full Name: ${profile.getName()}`);
+    console.debug(`Given Name: ${profile.getGivenName()}`);
+    console.debug(`Family Name: ${profile.getFamilyName()}`);
+    console.debug(`Image URL: ${profile.getImageUrl()}`);
+    console.debug(`Email: ${profile.getEmail()}`);
+
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.debug(`ID Token: ${id_token}`);
+  }
 </script>
 
 <div class="sign-in-div">
