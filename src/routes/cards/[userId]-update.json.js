ed9a42b4e410a9451c06ee6b50a761237525207c
Grant Yap <grantivanyap@yahoo.com>
2021-05-26 11:47:36 +0800

    Update `session.calendars`
    This patch also improves null safety for our `session` variables by
    letting them be empty arrays by default.

--- src/routes/cards/[userId]-update.json.js
+++ src/routes/cards/[userId]-update.json.js
@@ -2,18 +2,16 @@ import { updateCardsOfUser } from "./_database.js";
 
 export async function post(req, res) {
   const { userId } = req.params;
-  
-  req.session.lists = req.body.lists;
-  req.session.archived_cards = req.body.archived;
+
+  req.session.lists = req.body.lists ?? [];
+  req.session.archived_cards = req.body.archived_cards ?? [];
+  req.session.calendars = req.body.calendars ?? [];
 
   const lists = req.session.lists;
   const archivedCards = req.session.archived_cards;
+  const calendars = req.session.calendars;
 
-  const result = await updateCardsOfUser(userId, lists, archivedCards);
+  await updateCardsOfUser(userId, lists, archivedCards, calendars);
 
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-
-  res.end(JSON.stringify(result));
+  res.end();
 }
