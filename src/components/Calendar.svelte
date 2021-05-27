7e8ba16fd00de2dceac87cce703e9bdf12df9061
Grant Yap <grantivanyap@yahoo.com>
2021-05-27 16:30:17 +0800

    Implement initial layout for Calendar

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -1,6 +1,27 @@
 <script>
+  import { stores } from "@sapper/app";
+  import { Container } from "sveltestrap";
+  import FullCalendar from "svelte-fullcalendar";
+  import dayGridPlugin from "@fullcalendar/daygrid";
+
+  const { session } = stores();
+
+  let options = { initialView: "dayGridMonth", plugins: [dayGridPlugin], height: "100%" };
 </script>
 
-<div>
-  <h1>this is the calendar view</h1>
+<div class="calendar-flex-box-container">
+  <Container style="height: 100%;">
+    <FullCalendar {options} />
+  </Container>
 </div>
+
+<style>
+  .calendar-flex-box-container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    height: 100%;
+  }
+</style>
