d605daf36ec54863ddfac42d265d97b6c5581d82
Grant Yap <grantivanyap@yahoo.com>
2021-06-03 17:46:38 +0800

    Show Cards in Folders as events in Calendar
    Since Cards in Folders live one level deeper than Cards in Lists, we
    have to consider that when searching for the Card to show.
    
    We also clean up and separate some of the logic to make the code easier
    to understand.

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -8,34 +8,43 @@
   const { session } = stores();
   const selectedCard = writable(null);
 
-  $: eventsFromCards = () => {
+  const eventFromCard = (card) => {
+    const title = card.card_name !== "" ? card.card_name : card.original_title;
+    const start = new Date(
+      card.due_date_time !== "" ? card.due_date_time : card.original_date
+    );
+
+    // NOTE: By default, FullCalendar lets the duration of an event without
+    //       an end time to be one hour long. This means that events after
+    //       11:00 PM spill over into the next day.
+    //       To fix this, we just set each event to be one second long.
+    let end = new Date(start);
+    end.setSeconds(start.getSeconds() + 1);
+
+    const event = {
+      id: card._id,
+      title: title,
+      start: start,
+      end: end,
+      backgroundColor: card.color,
+      borderColor: "rgba(0, 0, 0, 0.125)",
+    };
+
+    return event;
+  };
+
+  const eventsFromCards = () => {
     let events = [];
 
     $session.lists.forEach((list) => {
-      list.cards.forEach((card) => {
-        const title =
-          card.card_name !== "" ? card.card_name : card.original_title;
-        const start = new Date(
-          card.due_date_time !== "" ? card.due_date_time : card.original_date
-        );
-
-        // NOTE: By default, FullCalendar lets the duration of an event without
-        //       an end time to be one hour long. This means that events after
-        //       11:00 PM spill over into the next day.
-        //       To fix this, we just set each event to be one second long.
-        let end = new Date(start);
-        end.setSeconds(start.getSeconds() + 1);
-
-        const event = {
-          id: card._id,
-          title: title,
-          start: start,
-          end: end,
-          backgroundColor: card.color,
-          borderColor: "rgba(0, 0, 0, 0.125)",
-        };
-
-        events = [...events, event];
+      list.cards.forEach((listElement) => {
+        if (listElement.folder_name != null) {
+          listElement.cards.forEach((card) => {
+            events = [...events, eventFromCard(card)];
+          });
+        } else {
+          events = [...events, eventFromCard(listElement)];
+        }
       });
     });
 
@@ -56,15 +65,25 @@
     events: events,
     eventClick: (eventClickInfo) => {
       for (const list of $session.lists) {
-        const result = list.cards.find(
-          (c) => c._id === eventClickInfo.event.id
-        );
+        let result = list.cards.find((c) => c._id === eventClickInfo.event.id);
         if (result) {
           $selectedCard = result;
-          return;
+        } else if (typeof result === "undefined") {
+          // We couldn't find the card in the first level. Look for it inside folders.
+          const folders = list.cards.filter((c) => c.folder_name != null);
+          for (const folder of folders) {
+            result = folder.cards.find(
+              (c) => c._id === eventClickInfo.event.id
+            );
+            if (result) {
+              $selectedCard = result;
+              break;
+            }
+          }
+        } else {
+          $selectedCard = null;
         }
       }
-      $selectedCard = null;
     },
     dayMaxEventRows: true,
     height: "100%",
