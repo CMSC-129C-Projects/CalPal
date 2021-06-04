b03fb21a6a42d84586d644dd312d250c21db2b12
Grant Yap <grantivanyap@yahoo.com>
2021-06-04 18:55:55 +0800

    Improve AddListButton to match new Bootstrap 5 styling
    This also gives it `:hover` and `:active` styles.

--- src/components/AddListButton.svelte
+++ src/components/AddListButton.svelte
@@ -11,23 +11,44 @@
       color="light"
       on:click={() => onClick()}
     >
-      Add List
-      <Icon class="add-list-button-plus-icon" name="plus" />
+      <div class="add-list-button-text">
+        <span> Add List </span>
+        <Icon class="add-list-button-plus-icon" name="plus" />
+      </div>
     </Button>
   </span>
 </div>
 
 <style>
   .add-list-button-parent :global(.add-list-button-add-list) {
-    height: 56px;
-    width: 250px;
+    height: calc(
+      47px + 1px
+    ); /* Add 1px to include the bottom border of CardHeader */
+    width: 258px;
+    background-color: rgba(0, 0, 0, 0.03);
     border: 1px solid rgba(0, 0, 0, 0.125);
     border-radius: 0.25rem;
-    color: #aaa;
+    color: rgba(0, 0, 0, 0.5);
+    transition: background-color 100ms, color 100ms;
+  }
+
+  .add-list-button-parent :global(.add-list-button-add-list:hover) {
+    background-color: rgba(0, 0, 0, 0.125);
+    color: rgba(0, 0, 0, 0.75);
+  }
+
+  .add-list-button-parent :global(.add-list-button-add-list:active) {
+    background-color: rgba(0, 0, 0, 0.25);
+    color: rgba(0, 0, 0, 1);
   }
 
   .add-list-button-parent :global(.add-list-button-plus-icon) {
     font-size: 24px;
-    line-height: 0;
+  }
+
+  .add-list-button-text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
