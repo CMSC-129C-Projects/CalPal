168fe75547a03b841b006231e0c195c166fcc8e0
Grant Yap <grantivanyap@yahoo.com>
2021-06-17 18:08:39 +0800

    Redirect HTTP traffic to HTTPS
    One drive-by fix is that we now use the `SESSION_SECRET` environment
    variable when initializing the session.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import sirv from "sirv";
 import polka from "polka";
+import redirect from "@polka/redirect";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 import session from "express-session";
@@ -7,13 +8,15 @@ import { json } from "body-parser";
 import { createServer } from "https";
 import { readFileSync } from "fs";
 
-const { PORT, NODE_ENV, GOOGLE_OAUTH2_CLIENT_ID } = process.env;
+const { PORT, NODE_ENV, GOOGLE_OAUTH2_CLIENT_ID, SESSION_SECRET } = process.env;
 const dev = NODE_ENV === "development";
 
-const server = polka() // You can also use Express
+const server = polka()
   .use(
     session({
-      secret: "SuperSecretShouldNotBeCommitted",
+      secret: SESSION_SECRET,
+      httpOnly: true,
+      secure: true,
       resave: true,
       saveUninitialized: true,
       cookie: {
@@ -38,11 +41,16 @@ const server = polka() // You can also use Express
     })
   );
 
+const redirectServer = polka().get("*", (req, res) => {
+  redirect(res, `https://${req.headers.host}${req.url}`);
+});
+
 if (dev) {
   server.listen(PORT, (err) => {
     if (err) console.log("error", err);
   });
 } else {
+  const httpPort = 80;
   const sslPort = 443;
   const options = {
     key: readFileSync("/etc/letsencrypt/live/calpal.upcebu.edu.ph/privkey.pem"),
@@ -51,7 +59,10 @@ if (dev) {
     ),
   };
 
-  createServer(options, server.handler).listen(PORT, (_) => {
+  redirectServer.listen(httpPort, (err) => {
+    console.error(err);
+  });
+  createServer(options, server.handler).listen(sslPort, (_) => {
     console.log(`> Running on https://localhost:${sslPort}`);
   });
 }
